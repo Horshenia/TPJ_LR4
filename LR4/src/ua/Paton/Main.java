25e3e3dc2fe0f4ce0d9f2e5ac51b6bfeaddb87c4
Horshenia <leib-horshenia@ukr.net>
2022-10-03 16:04:30 +0300

    Оновлення

--- LR4/src/ua/Paton/Main.java
+++ LR4/src/ua/Paton/Main.java
@@ -3,6 +3,10 @@ package ua.Paton;
 public class Main {
 	
 	public static void main(String[] arg) {
-		
+		System.out.println("1)\nisSpaceChar - перевірює наявність пробелів");
+		System.out.println("Character.isSpaceChar(\'0\') = "+Character.isSpaceChar('0'));
+		System.out.println("2)\nРакета - 🚀(U+1F680)"
+				+ "\nУсміхнене обличчя - ☺(U+263A)"
+				+ "\nОбличчя в сонячних окулярах - 😎(U+1F60E)");
 	}
 }
